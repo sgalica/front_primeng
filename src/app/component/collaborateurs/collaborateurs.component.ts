b7a4f7a97eb1810127d25ad42d42faa82234462f
FBO14041 <farid.bouras@open-groupe.com>
2018-09-18 17:14:27 +0200

    first commit csv import

--- src/app/component/collaborateurs/collaborateurs.component.ts
+++ src/app/component/collaborateurs/collaborateurs.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {SelectItem} from 'primeng/api';
 import {first} from 'rxjs/operators';
 import {CollaborateurService} from '../../service/collaborateur.service';
 import {Collaborateur} from '../../model/collaborateur';
 import {NewCollaborateurComponent} from "../newCollaborateur/newCollaborateur.component";
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-collaborateurs',
@@ -34,24 +35,27 @@ export class CollaborateursComponent implements OnInit {
 
     ngOnInit() {
 
-        this.sortOptions = [
-            {label: 'Newest First', value: '!nom'},
-            {label: 'Oldest First', value: 'prenom'},
-            {label: 'Brand', value: 'brand'}
-        ];
+
         this.cols = [
 
-            {header: 'id_pilot', field:'id_pilot'},
-            {header: 'nom', field:'nom'},
-            {header: 'prenom', field:'prenom'},
-            {header: 'trig_open', field:'trig_open'},
-            {header: 'tel_perso', field:'tel_perso'},
-            {header: 'tel_pro', field:'tel_pro'},
-            {header: 'mail_open', field:'mail_open'},
-            {header: 'mail_sg', field:'mail_sg'},
-            {header: 'categorisation', field:'code_categorisation'},
-            {header: 'top_statut', field:'top_statut'},
-            {header: 'statut', field:'statut'},
+            {header: 'trig_open'        , field:'trig_open'          },
+            {header: 'nom'              , field:'nom'                },
+            {header: 'prenom'           , field:'prenom'             },
+            {header: 'tel_perso'        , field:'tel_perso'          },
+            {header: 'tel_pro'          , field:'tel_pro'            },
+            {header: 'mail_open'        , field:'mail_open'          },
+            {header: 'mail_sg'          , field:'mail_sg'            },
+            {header: 'categorisation'   , field:'code_categorisation'},
+            {header: 'top_statut'       , field:'top_statut'         },
+            {header: 'statut_Collab'    , field:'statut_Collab'      },
+            {header: 'version_Collab'   , field:'version_Collab'     },
+            {header: 'societe_stt'      , field:'societe_stt'        },
+            {header: 'pre_embauche'     , field:'pre_embauche'       },
+            {header: 'date_embauche'    , field:'date_embauche'      },
+            {header: 'created_at'       , field:'created_at'         },
+            {header: 'created_by'       , field:'created_by'         },
+            {header: 'updated_at'       , field:'updated_at'         },
+            {header: 'updated_by'       , field:'updated_by'         },
 
         ]
 
@@ -106,6 +110,103 @@ export class CollaborateursComponent implements OnInit {
        this.collaborateurService.update(collaborateur);
 
 
+    }
+
+    /************************************************************************************************************/
+
+
+    public csvRecords: any[] = [];
+
+    @ViewChild('fileImportInput') fileImportInput: any;
+
+
+    fileChangeListener($event: any): void {
+
+        var text = [];
+        var files = $event.srcElement.files;
+
+        if (this.isCSVFile(files[0])) {
+
+            var input = $event.target;
+            var reader = new FileReader();
+            reader.readAsText(input.files[0]);
+
+            reader.onload = (data) => {
+                let csvData = reader.result;
+                let csvRecordsArray = csvData.split(/\r\n|\n/);
+
+                let headersRow = this.getHeaderArray(csvRecordsArray);
+
+                this.csvRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+            }
+
+            reader.onerror = function () {
+                alert('Unable to read ' + input.files[0]);
+            };
+
+        } else {
+            alert("Please import valid .csv file.");
+            this.fileReset();
+        }
+    }
+
+    getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+        var dataArr = []
+
+        for (let i = 1; i < csvRecordsArray.length; i++) {
+            let data = csvRecordsArray[i].split(',');
+
+            // FOR EACH ROW IN CSV FILE IF THE NUMBER OF COLUMNS
+            // ARE SAME AS NUMBER OF HEADER COLUMNS THEN PARSE THE DATA
+            if (data.length == headerLength) {
+                var csvRecord: Collaborateur = new Collaborateur();
+                Object.keys(csvRecord).map(function(key, index) {csvRecord[key] = data[5]})
+                //alert(csvRecord.nom);
+                dataArr.push(csvRecord);
+
+                /*                csvRecord.id = data[0]                          ;
+                                csvRecord.trig_open = data[3]                   ;
+                                csvRecord.nom = data[1]                         ;
+                                csvRecord.prenom = data[2]                      ;
+                                csvRecord.tel_perso = data[4]                   ;
+                                csvRecord.tel_pro = data[5]                     ;
+                                csvRecord.mail_open = data[6]                   ;
+                                csvRecord.mail_sg = data[7]                     ;
+                                csvRecord.code_categorisation = data[8]         ;
+                                csvRecord.top_statut = data[9]                  ;
+                                csvRecord.statut_Collab = data[10]              ;
+                                csvRecord.version_Collab = data[11]             ;
+                                csvRecord.societe_stt = data[12]                ;
+                                csvRecord.pre_embauche = data[13]               ;
+                                csvRecord.date_embauche = data[14]              ;
+                                csvRecord.created_at = data[15]                 ;
+                                csvRecord.created_by = data[16]                 ;
+                                csvRecord.updated_at = data[17]                 ;
+                                csvRecord.updated_by = data[18]                 ;*/
+
+            }
+        }
+        return dataArr;
+    }
+
+    // CHECK IF FILE IS A VALID CSV FILE
+    isCSVFile(file: any) {
+        return file.name.endsWith(".csv");
+    }
+
+    // GET CSV FILE HEADER COLUMNS
+    getHeaderArray(csvRecordsArr: any) {
+        let headers = csvRecordsArr[0].split(',');
+        let headerArray = [];
+        for (let j = 0; j < headers.length; j++) {
+            headerArray.push(headers[j]);
+        }
+        return headerArray;
+    }
+
+    fileReset() {
+        this.fileImportInput.nativeElement.value = "";
+        this.csvRecords = [];
     }
 
 }
--- src/app/model/collaborateur.ts
+++ src/app/model/collaborateur.ts
@@ -1,13 +1,21 @@
 export class Collaborateur {
-    id_pilot;
-    nom;
-    prenom;
-    trig_open;
-    tel_perso;
-    tel_pro;
-    mail_open;
-    mail_sg;
-    code_categorisation;
-    top_statut;
-    statut;
+     id                   : any;
+     trig_open            : any;
+     nom                  : any;
+     prenom               : any;
+     tel_perso            : any;
+     tel_pro              : any;
+     mail_open            : any;
+     mail_sg              : any;
+     code_categorisation  : any;
+     top_statut           : any;
+     statut_Collab        : any;
+     version_Collab       : any;
+     societe_stt          : any;
+     pre_embauche         : any;
+     date_embauche        : any;
+     created_at           : any;
+     created_by           : any;
+     updated_at           : any;
+     updated_by           : any;
 }
