8124adb86f5228ad21cec4e55d8db222c66ee6a7
FBO14041 <farid.bouras@open-groupe.com>
2018-10-09 15:36:31 +0200

    message success managed 11 straight foward method to fix ts-xlsx import

--- src/app/service/auth.guard.ts
+++ src/app/service/auth.guard.ts
@@ -1,8 +1,6 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, CanActivateChild, Router, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {BehaviorSubject, Observable} from 'rxjs/Rx';
-import {decode} from "punycode";
-import {log} from "util";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -39,12 +37,12 @@ export class AuthGuard implements CanActivate {
 
         if (localStorage.getItem('currentUser')) {
 
-            // this will be passed from the route config
+           /* // this will be passed from the route config
             // on the data property
             const token = localStorage.getItem('accessToken');
             // decode the token to get its payload
             const tokenPayload = decode(token);
-            console.log("USER ROLE :::::::::::::::::::::", token);
+            console.log("USER ROLE :::::::::::::::::::::", token);*/
             AuthGuard.logged.next(true);
         } else {
             AuthGuard.logged.next(false);
