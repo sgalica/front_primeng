ce4b227873ceebe6a5bd257e560890118fb01854
FBO14041 <farid.bouras@open-groupe.com>
2018-10-09 14:20:24 +0200

    admin role management

---
+++ src/app/service/admin.guard.ts
@@ -0,0 +1,24 @@
+import {
+    Injectable
+} from '@angular/core';
+import {
+    CanActivate,
+    Router
+} from '@angular/router';
+
+@Injectable()
+export class AdminIsLoggedInGuard implements CanActivate {
+
+    constructor(private router: Router,private adminservice: AdminService) {}
+
+    canActivate() {
+        if (this.adminservice.AdminIsLoggedIn()) {
+            return true;
+        } else {
+            alert("U mag deze pagina niet bezichtigen");
+            this.router.navigate(['/']);
+            return false;
+        }
+    }
+
+}
